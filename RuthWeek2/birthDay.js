6fac55280035cc87afab652dec59d42cabd4e79f
Ruth Mwangi <rmwangi22020@gmail.com>
2022-10-21 14:07:25 +0300

    js code update

---
+++ RuthWeek2/birthDay.js
@@ -0,0 +1,25 @@
+const submit = () => {
+// retrieve elements
+const dateElement = document.getElementById("date")
+const maleElement = document.getElementById("male")
+const femaleElement = document.getElementById("female")
+
+// retrieving input values
+
+const date = dateElement.value; 
+const male = maleElement.value;
+const female = femaleElement.value
+
+
+console.log(male)
+
+
+
+
+
+
+
+
+
+
+}
